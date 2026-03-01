10ac54b035167f24e6345413cc7862ecb62c7808
lakshyaprajapat6990 <you@example.com>
2026-03-01 22:19:25 +0530

    nity

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const connectDB = require("./config/db");
+const { connectDB } = require("./config/db");
 const { initializeDatabase } = require('./config/initBehaviorDB');
 const cors = require("cors");
 const path = require("path");
